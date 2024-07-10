5dd3d8c7f144ba5b5927f85af39523c85a8d7312
om <omejjigiri99@gmail.com>
2024-07-10 22:58:44 +0530

    commit

--- frontend.sh
+++ frontend.sh
@@ -1,38 +1,40 @@
-echo -e "\e[32m installing the nginx \e[0m" 
+log_file = &>>/tmp/expense.log
+color = \e[32m
 
-dnf install nginx -y &>>/tmp/expense.log
+echo -e "${color} installing the nginx \e[0m" 
 
+dnf install nginx -y &>>$log_file
 echo $?
 
-echo -e "\e[32m copy expense config file \e[0m"
+echo -e "${color}copy expense config file \e[0m"
 
-cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 echo $?
 
-echo -e "\e[32m removing the file \e[0m"
+echo -e "${color} removing the file \e[0m"
 
-rm -rf /usr/share/nginx/html/*  &>>/tmp/expense.log
+rm -rf /usr/share/nginx/html/*  &>>$log_file
 echo $?
 
-echo -e "\e[32m removing the file \e[0m"
+echo -e "${color} removing the file \e[0m"
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 echo $?
 
-echo -e "\e[32m changing the directory \e[0m"
+echo -e "${color} changing the directory \e[0m"
 
-cd /usr/share/nginx/html  &>>/tmp/expense.log
+cd /usr/share/nginx/html  &>>$log_file
 echo $?
 
-echo -e "\e[32m unzip the file \e[0m"
-unzip /tmp/frontend.zip &>>/tmp/expense.log
+echo -e "${color} unzip the file \e[0m"
+unzip /tmp/frontend.zip &>>$log_file
 echo $?
 
-echo -e "\e[32m Enable & restart the nginx \e[0m"
+echo -e "${color} Enable & restart the nginx \e[0m"
 echo $?
 
-systemctl enable nginx  &>>/tmp/expense.log
-systemctl restart nginx &>>/tmp/expense.log
+systemctl enable nginx  &>>$log_file
+systemctl restart nginx &>>$log_file
 
 echo $?
 
